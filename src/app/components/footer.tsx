bd2b3abba0290b101409cffc0f315e706a944c78
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2023-05-13 21:20:38 -0500

    added footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React, { ReactElement } from 'react';
+import { Text } from '@chakra-ui/react';
+
+const Footer = (): ReactElement => {
+    return(
+        <Text align="center">All Rights Reserved - Daniel Pericich 2023 &copy;</Text>
+    )
+};
+
+export default Footer;
