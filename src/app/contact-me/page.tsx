902c04edc0d00ce658a8fdfbb79681aad2ee1deb
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2023-05-13 21:21:46 -0500

    stubbed contact me page component

---
+++ src/app/contact-me/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React, { ReactElement } from 'react';
+
+const ContactPage = (): ReactElement => {
+    return(
+        <div>Hello</div>
+    );
+};
+
+export default ContactPage;
