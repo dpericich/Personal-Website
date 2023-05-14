3e0947d3bce1d7cb857688a3d198f8be014249bf
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2023-05-13 21:26:59 -0500

    stubbed in blog page component

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React, { ReactElement } from 'react';
+
+const BlogPage = () => {
+    return(
+        <div>Hello Daniel, Welcome to the Blog!</div>
+    )
+};
+
+export default BlogPage;
