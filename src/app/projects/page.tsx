d406d8fbd2189dd5230bd7d64530993be148502b
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2023-05-13 21:21:32 -0500

    stubbed projects page component

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+import React, { ReactElement } from 'react';
+
+// My Projects
+// Advent Calendar
+// Clarity Camp
+// Web3 Calculator
+// Blog Posts w/ Chegg
+// Fill Rite -> at some point
+
+const ProjectsPage = (): ReactElement => {
+    return(
+        <div>These are where my projects will live</div>
+    )
+};
+
+export default ProjectsPage;
