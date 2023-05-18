2bd997ebe71db5f23a15669eaf2443bf8e81a400
Daniel Pericich <dpericich@gmail.com>
2023-05-18 17:36:51 -0500

    scaffolded preview card for different pages

---
+++ src/app/components/previewCard.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { Flex } from '@chakra-ui/react';
+import React, { ReactElement } from 'react';
+// Need to pull in the data for these previews here
+
+interface previewCardProps {
+    // image path
+    // title
+    // descriptoin
+    // link text
+    // link path    
+}
+
+const previewCard = (): ReactElement => {
+    return(
+        <Flex></Flex>
+    )
+};
+
+export default previewCard;
