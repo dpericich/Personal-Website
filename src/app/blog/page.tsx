48f6c7a0ad3fd415fd5d674120a3e81d4871adbc
Daniel Pericich <dpericich@gmail.com>
2023-05-18 17:37:42 -0500

    added blog page scaffolding and notes to integrate Medium API into the page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,18 @@
 'use client'
-import React, { ReactElement } from 'react';
+import React, { useState, useEffect, ReactElement } from 'react';
 
+// I will pull in all the articles pulled from the 
+useEffect(() => {
+    // will setArticleData here on page load
+}, [])
+
+const [articleData, setArticleData] = useState([]);
+
+// No pagination, just do infinite scroll
+// https://www.digitalocean.com/community/tutorials/react-react-infinite-scroll
+
+// I will use this API for getting data:
+// https://docs.mediumapi.com/
 const BlogPage = () => {
     return(
         <div>Hello Daniel, Welcome to the Blog!</div>
