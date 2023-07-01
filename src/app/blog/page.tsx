dfa93362f05b8b8a10ee13301be6914a971f4058
Daniel Pericich <dpericich@gmail.com>
2023-07-01 12:42:31 -0500

    refactored blog page to use PreviewCard component

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,22 +1,33 @@
 'use client'
-import React, { useState, useEffect, ReactElement } from 'react';
-
-// I will pull in all the articles pulled from the 
-useEffect(() => {
-    // will setArticleData here on page load
-}, [])
-
-const [articleData, setArticleData] = useState([]);
+import React, { ReactElement } from 'react';
+import { Heading, Flex } from '@chakra-ui/react'
+import { blogData } from "../../api/data/blogData.json"
+import PreviewCard from '../../../components/PreviewCard';
 
 // No pagination, just do infinite scroll
+// Figure out how to do 
 // https://www.digitalocean.com/community/tutorials/react-react-infinite-scroll
 
 // I will use this API for getting data:
 // https://docs.mediumapi.com/
-const BlogPage = () => {
+
+const BlogPage = (): ReactElement => {
     return(
-        <div>Hello Daniel, Welcome to the Blog!</div>
-    )
+        <div>
+            <Heading as="h1" size="4xl" marginLeft="2rem">Recent Blog Posts by Daniel</Heading>
+            <Flex flexDir="column" padding="2rem 0">
+                {blogData.map(item => {
+                    return <PreviewCard 
+                        id={item.id} 
+                        title={item.title} 
+                        description={item.excerpt} 
+                        imagePath={item.blogImage} 
+                        linkPath={item.linkPath} 
+                    />
+                })}
+            </Flex>
+        </div>
+    );
 };
 
 export default BlogPage;
