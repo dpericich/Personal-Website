4a4c79c22e57a9a550eef2a536804d60b7d0a4c1
Daniel Pericich <dpericich@gmail.com>
2023-07-04 17:32:37 -0500

    adding input fields to contact page

--- src/app/contact-me/page.tsx
+++ src/app/contact-me/page.tsx
@@ -1,9 +1,28 @@
 'use client'
 import React, { ReactElement } from 'react';
+import { Flex, Heading, Input, Textarea, Button } from '@chakra-ui/react';
 
 const ContactPage = (): ReactElement => {
+    // TODO: Need to add controlled inputs
     return(
-        <div>Do you want to build a shop for your business, need to launch a new API, or have a stubborn bug that is </div>
+        <>
+            <Heading as="h1">Contact Me</Heading>
+            <Flex flexDir="column" backgroundColor="white" color="black" padding="2rem">
+                <Flex flexDir="column" justifyContent="center" alignItems="center" >
+                    <Flex justifyContent="center" alignContent="center">
+                        <Input placeholder='First Name' padding="0.75rem 1.5rem" fontSize="1.5rem" />
+                        <Input placeholder='Last Name' padding="0.75rem 1.5rem" fontSize="1.5rem" />
+                    </Flex>
+                    <Input placeholder='Email' padding="0.75rem 1.5rem" fontSize="1.5rem" />
+                    <Textarea 
+                        placeholder="Tell me a little bit about who you are and what you're looking for!" 
+                        padding="0.75rem 1.5rem" 
+                        fontSize="1.5rem"
+                    />
+                    <Button size="lg" variant="outline">Submit</Button>
+                </Flex>
+            </Flex>
+        </>
     );
 };
 
